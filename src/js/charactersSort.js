ee7eace31a06360ca8a94125c3b20adf629e1895
Logot1n <miwutka2@gmail.com>
2023-05-07 21:32:47 +0300

    Eslint update

--- src/js/charactersSort.js
+++ src/js/charactersSort.js
@@ -1,4 +1,4 @@
 export default function charactersSort(characters) {
-    let newArray = characters.sort((a, b) => b.health - a.health);
-    return newArray;
+  const newArray = characters.sort((a, b) => b.health - a.health);
+  return newArray;
 }
--- src/test/test.js
+++ src/test/test.js
@@ -2,15 +2,15 @@ import charactersSort from '../js/charactersSort';
 
 test('checking characters sort', () => {
   const characters = [
-      {name: 'мечник', health: 10},
-      {name: 'маг', health: 100},
-      {name: 'лучник', health: 80},
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
   ];
   const expected = [
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
-    {name: 'мечник', health: 10},
-  ]
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
   const result = charactersSort(characters);
   expect(result).toEqual(expected);
-})
+});
